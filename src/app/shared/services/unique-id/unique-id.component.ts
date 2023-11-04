7047e55d7c5d5798c0a2b000c919c01dedd282e3
caueeugenio <caue_eugenio@hotmail.com>
2023-11-04 19:26:09 -0300

    04-11-23: updating branch

--- src/app/shared/services/unique-id/unique-id.component.ts
+++ src/app/shared/services/unique-id/unique-id.component.ts
@@ -4,6 +4,8 @@ import {v4 as uuidv4} from 'uuid';
 @Injectable()
 export class UniqueIdService {
   private numberOfGeneratedIds = 0;
+  // Testa se é seguido de letra maiscula ou minuscula e se tem hifen e dps vem qualquer outra coisa
+  private validId = /^[A-Za-z]+[\w\-\:\.]*$/;
   public generateUniqueIdWithPrefix(prefix: string): string {
     if(!prefix) {
       throw Error("Prefix can't be empty");
--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,12 +1,43 @@
-import { UniqueIdService } from "./unique-id.component";
+import { UniqueIdService } from './unique-id.component';
 
-describe(UniqueIdService.name,  () => {
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-    const service = new UniqueIdService(); // instancia da service
-    const id = service.generateUniqueIdWithPrefix('app');
-    expect(id).toContain('app-');
+describe(UniqueIdService.name, () => {
+  let service: UniqueIdService = null;
+
+  beforeEach(() => {
+    /**
+     * beforeEach -> Tem como papel principal garantir que cada
+     * chamada à função it tenha seu próprio conjunto de dados de testes.
+     * OBS: Isso evita que mudança de estado no conjunto de dados de teste vaze para outro.
+     */
+    service = new UniqueIdService();
   });
-
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should generate id when called with prefix`, () => {
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should not generate duplicate IDS when called multitple times`, () => {
+    const ids = new Set(); // set nao aceita elementos duplicados, ele ignora a adição
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    should return the number of generateIds when called`, () => {
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
+  });
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    should throw when called with empty`, () => {
+      // Quando temos que testar uma exceção precisamos declarar uma função dentro do expect
+    const emptyValues = [null, undefined, ''];
+    emptyValues.forEach((emptyValue) => {
+      expect(() => service.generateUniqueIdWithPrefix(emptyValue)).toThrow();
+    });
+  });
 });
 
 // --> existe uma convenção que colocando o # antes do it indica um metodo
